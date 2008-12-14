95ef4153a15e3f8a54e5522ddeb94f0e16edc4a6
George Brocklehurst <george.brocklehurst@gmail.com>
2008-12-14 11:46:38 +0000

    Set up Info.plist to be a SIMBL plugin

---
+++ SpotifyScrobbler.h
@@ -0,0 +1,17 @@
+//
+//  SpotifyScrobbler.h
+//  SpotifyScrobbler
+//
+//  Created by George on 14/12/2008.
+//  Copyright 2008 George Brocklehurst. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface SpotifyScrobbler : NSObject 
+{
+
+}
+
+@end
---
+++ SpotifyScrobbler.m
@@ -0,0 +1,14 @@
+//
+//  SpotifyScrobbler.m
+//  SpotifyScrobbler
+//
+//  Created by George on 14/12/2008.
+//  Copyright 2008 George Brocklehurst. All rights reserved.
+//
+
+#import "SpotifyScrobbler.h"
+
+
+@implementation SpotifyScrobbler
+
+@end
