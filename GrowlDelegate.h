b9bacf577774f8e0618b124c7af160c544cd376b
George Brocklehurst <george.brocklehurst@gmail.com>
2008-12-15 01:57:36 +0000

    Added a LICENSE file and changed default "All rights reserved" message in source files

--- GrowlDelegate.h
+++ GrowlDelegate.h
@@ -3,7 +3,7 @@
 //  SpotifyScrobbler
 //
 //  Created by George on 14/12/2008.
-//  Copyright 2008 George Brocklehurst. All rights reserved.
+//  Copyright 2008 George Brocklehurst. Some rights reserved (see accompanying LICENSE file for details).
 //
 
 #import <Cocoa/Cocoa.h>
